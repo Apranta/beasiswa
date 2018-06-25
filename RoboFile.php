206ace7302568fc177dcaadb5668df153c7be67e
azhry <arliansyah_azhary@yahoo.com>
2018-06-26 00:39:33 +0700

    Added more task command

--- RoboFile.php
+++ RoboFile.php
@@ -11,4 +11,12 @@ class RoboFile extends \Robo\Tasks
     	$this->say("Menjalakan perintah dengan parameter: " . $arg);
     	$this->_exec("git " . $arg);
     }
+
+    public function push($commitMessage, $branch = "master")
+    {
+    	$this->say("Pushing code to repository...");
+    	$this->_exec("git add .");
+    	$this->_exec("git commit -m \"" . $commitMessage . "\"");
+    	$this->_exec("git push origin " . $branch);
+    }
 }
