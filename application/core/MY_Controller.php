0013de90e124bf8002cb8b6203e6bce84dfc6bb3
azhry <arliansyah_azhary@yahoo.com>
2018-07-10 11:03:42 +0700

    Added output compressor

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -177,7 +177,31 @@ class MY_Controller extends CI_Controller
             '\\1',
             ''
         ];
-        $buffer = preg_replace($search, $replace, $buffer);
+        
+        libxml_use_internal_errors(true);
+        $doc = new DOMDocument();
+        $doc->loadHTML($buffer);
+        $head = $doc->getElementsByTagName('head');
+        if (isset($head[0])) {
+            $analytics = "
+        		<!-- Google Analytics -->
+        		<script>
+				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+				ga('create', 'UA-XXXXX-Y', 'auto');
+				ga('send', 'pageview');
+				</script>
+				<!-- End Google Analytics -->"; // replace UA-XXXXX-Y with your tracking ID
+
+            $fragment = $doc->createDocumentFragment();
+            $fragment->appendXML($analytics);
+            $head[0]->appendChild($fragment);
+            $buffer = preg_replace($search, $replace, $doc->saveHTML());
+        }
+        
         return $buffer;
     }
 }
